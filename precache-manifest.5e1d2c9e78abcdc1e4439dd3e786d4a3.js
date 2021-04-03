299826b8f70026abab99a744c87ce705c889c539
Michal Popielarz <michael010pl@gmail.com>
2021-04-03 18:25:01 +0200

    Updates

---
+++ precache-manifest.5e1d2c9e78abcdc1e4439dd3e786d4a3.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = [
+  {
+    "revision": "722041a5101bbe7c935a",
+    "url": "/static/css/main.f2fa2a75.chunk.css"
+  },
+  {
+    "revision": "722041a5101bbe7c935a",
+    "url": "/static/js/main.33c71bd5.chunk.js"
+  },
+  {
+    "revision": "26fc67b71635cd49e513",
+    "url": "/static/js/runtime~main.1d157d3c.js"
+  },
+  {
+    "revision": "697b94c010a4691dc79b",
+    "url": "/static/css/2.9c6e78b8.chunk.css"
+  },
+  {
+    "revision": "697b94c010a4691dc79b",
+    "url": "/static/js/2.b01dc6e7.chunk.js"
+  },
+  {
+    "revision": "b034e2b166fba6b6444b",
+    "url": "/static/js/3.1257caa2.chunk.js"
+  },
+  {
+    "revision": "b65ffd7688ac8faec05dc7f0d994a32b",
+    "url": "/static/media/unityLogo.b65ffd76.png"
+  },
+  {
+    "revision": "466c019115395fdc70327742f1a753d3",
+    "url": "/static/media/blender_logo.466c0191.png"
+  },
+  {
+    "revision": "8c33573c41fd1af4b42518a46820f9d0",
+    "url": "/static/media/inkscape-logo.8c33573c.svg"
+  },
+  {
+    "revision": "9ca6c090e2c0151e986425b694217529",
+    "url": "/static/media/SeonerLogo_White.9ca6c090.svg"
+  },
+  {
+    "revision": "5fcd520193a976dbb5f5e1d2aa79c294",
+    "url": "/static/media/C_Sharp_logo.5fcd5201.svg"
+  },
+  {
+    "revision": "614e81bd92afc62be8127beac5e1b9af",
+    "url": "/static/media/cpp_logo.614e81bd.png"
+  },
+  {
+    "revision": "ba69aa594be5fab39d2fc2d0ec60f8f0",
+    "url": "/static/media/VisualStudioLogo.ba69aa59.svg"
+  },
+  {
+    "revision": "f0c0dbd57e5bfcc010875044c721edec",
+    "url": "/static/media/MaterializeLogo.f0c0dbd5.png"
+  },
+  {
+    "revision": "b1547f30407a6a02e1d2c7ad2d555721",
+    "url": "/static/media/gimpLogo.b1547f30.png"
+  },
+  {
+    "revision": "eb6af79c15a2079db7e6d6457ad78b49",
+    "url": "/static/media/skyPart.eb6af79c.png"
+  },
+  {
+    "revision": "4a0ec363e5204ce82df49c9e93a2c439",
+    "url": "/index.html"
+  }
+];
